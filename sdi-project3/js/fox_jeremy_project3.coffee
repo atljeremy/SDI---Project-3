8667383d18be0f466ccb79d01984b5ae17440842
Jeremy Fox <atljeremy@me.com>
2011-12-06 18:48:37 -0500

    Beginnings to my project 3. Changed a few file names and added a couple files.

---
+++ sdi-project3/js/fox_jeremy_project3.coffee
@@ -0,0 +1,5 @@
+# Deliverable 3
+# Author: Jeremy Fox
+# Created For: SDI Online
+# Implementation of the Project 3 flowchart
+
---
+++ sdi-project3/js/json.js
@@ -0,0 +1,19 @@
+// Deliverable 3 (JSON)
+// Author: Jeremy Fox
+// Created For: SDI Online
+// JSON Object for use in Project 3
+
+var JSON = {
+  "TopLevel": {
+    "InnerLever": {
+      "property1":"prop1",
+      "property2":"prop2",
+      "property3":"prop3"
+    }
+    "InnerLevel2": {
+      "property1":"prop1",
+      "property2":"prop2",
+      "property3":"prop3"
+    }
+  }
+}
